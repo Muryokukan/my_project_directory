15a9ff5802cc06ebd3b463469734b6f1c1755bf7
Cédric <c.limosin@outlook.fr>
2024-11-20 16:44:58 +0100

    Troisième
    oé

--- src/Controller/TeamController.php
+++ src/Controller/TeamController.php
@@ -2,12 +2,27 @@
 
 namespace App\Controller;
 
+
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-class TeamController
+use App\Repository\TeamRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Serializer\SerializerInterface;
+
+
+class TeamController extends AbstractController
 {
+    #[Route('/api/teams', name: 'api_get_team', methods: ['GET'])]
+    public function teamGet(TeamRepository $teamRepository, SerializerInterface $serializerInterface): JsonResponse
+    {
+        $team = $teamRepository->findAll();
+        $teamJson = $serializerInterface->serialize($team,'json', ['groups'=>'allteam']);
+
+        return new JsonResponse($teamJson, 200, [], true);
+    }
+
     #[Route('/api/teams', name: 'api_create_team', methods: ['POST'])]
     public function createTeam(Request $request): JsonResponse
     {
